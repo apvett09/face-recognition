f40ae3f7e808ac7bb83adc08848dab931e8a6824
Anthony Vettraino <apvett@umich.edu>
2023-07-19 23:17:58 -0400

    Update Signin.js and Register.js with hooks

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,36 +1,31 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class Register extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: "",
-      password: "",
-      name: "",
-      error: "",
-    };
-  }
+const Register = ({ onRouteChange, loadUser }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-  onNameChange = (event) => {
-    this.setState({ name: event.target.value });
+  const onNameChange = (event) => {
+    setName(event.target.value);
   };
 
-  onEmailChange = (event) => {
-    this.setState({ email: event.target.value });
+  const onEmailChange = (event) => {
+    setEmail(event.target.value);
   };
 
-  onPasswordChange = (event) => {
-    this.setState({ password: event.target.value });
+  const onPasswordChange = (event) => {
+    setPassword(event.target.value);
   };
 
-  onSubmitSignIn = () => {
+  const onSubmitSignIn = () => {
     fetch("https://vert-fromage-72945-b3ad89284d69.herokuapp.com/register", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        email: this.state.email,
-        password: this.state.password,
-        name: this.state.name,
+        email: email,
+        password: password,
+        name: name,
       }),
     })
       .then((response) => {
@@ -45,75 +40,72 @@ class Register extends Component {
       })
       .then((user) => {
         if (user.id) {
-          this.props.loadUser(user);
-          this.props.onRouteChange("home");
+          loadUser(user);
+          onRouteChange("home");
         }
       })
       .catch((error) => {
         // console.log(error);
-        this.setState({ error: error.message }); // Update the error state
+        setError(error.message); // Update the error state
       });
   };
 
-  render() {
-    const { error } = this.state; // Get the error from the state
-    return (
-      <article className="br3 ba shadow-5 b--black-10 mv4 w-100 w-50-m w-25-l mw6 center">
-        <main className="pa4 black-80">
-          <div className="measure">
-            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-              <legend className="f1 fw6 ph0 mh0">Register</legend>
-              <div className="mt3">
-                <label className="db fw6 lh-copy f6" htmlFor="name">
-                  Name
-                </label>
-                <input
-                  onChange={this.onNameChange}
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                  type="text"
-                  name="name"
-                  id="name"
-                />
-              </div>
-              <div className="mt3">
-                <label className="db fw6 lh-copy f6" htmlFor="email-address">
-                  Email
-                </label>
-                <input
-                  onChange={this.onEmailChange}
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                  type="email"
-                  name="email-address"
-                  id="email-address"
-                />
-              </div>
-              <div className="mv3">
-                <label className="db fw6 lh-copy f6" htmlFor="password">
-                  Password
-                </label>
-                <input
-                  onChange={this.onPasswordChange}
-                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                  type="password"
-                  name="password"
-                  id="password"
-                />
-              </div>
-            </fieldset>
-            <div className="">
+  return (
+    <article className="br3 ba shadow-5 b--black-10 mv4 w-100 w-50-m w-25-l mw6 center">
+      <main className="pa4 black-80">
+        <div className="measure">
+          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+            <legend className="f1 fw6 ph0 mh0">Register</legend>
+            <div className="mt3">
+              <label className="db fw6 lh-copy f6" htmlFor="name">
+                Name
+              </label>
               <input
-                onClick={this.onSubmitSignIn}
-                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-                type="submit"
-                value="Register"
+                onChange={onNameChange}
+                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="text"
+                name="name"
+                id="name"
               />
             </div>
-            {error && <p className="errorMessage">{error}</p>}
+            <div className="mt3">
+              <label className="db fw6 lh-copy f6" htmlFor="email-address">
+                Email
+              </label>
+              <input
+                onChange={onEmailChange}
+                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="email"
+                name="email-address"
+                id="email-address"
+              />
+            </div>
+            <div className="mv3">
+              <label className="db fw6 lh-copy f6" htmlFor="password">
+                Password
+              </label>
+              <input
+                onChange={onPasswordChange}
+                className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="password"
+                name="password"
+                id="password"
+              />
+            </div>
+          </fieldset>
+          <div className="">
+            <input
+              onClick={onSubmitSignIn}
+              className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+              type="submit"
+              value="Register"
+            />
           </div>
-        </main>
-      </article>
-    );
-  }
-}
+          {error && <p className="errorMessage">{error}</p>}
+        </div>
+      </main>
+    </article>
+  );
+};
 
 export default Register;
